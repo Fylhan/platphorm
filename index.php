6c0d125d261fb731e6fd7ac86c1ab60e61ca606a
Olivier Maridat <olivier.maridat@trialog.com>
2013-04-05 13:18:29 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<title>Platphorm</title>
+	<meta name="author" content="Fylhan" />
+</head>
+<body>
+	<p>Hello World!</p>
+</body>
+</html>
