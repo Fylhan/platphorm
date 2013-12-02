df608b919ba63aad691c9cdba7b0c8745f9ee97b
Fylhan <bnmaster@la-bnbox.fr>
2013-12-02 13:39:17 +0000

    Begin to use Composer component to retrieve packages

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-platphorm
+Platphorm
 =========
 
 Platphorm is a proof of concept of a PHP platform able to install gracefully PHP apps (like SHaarli, Blogotext, Leed and KrissFeed) using Composer.
+
+
+Work in progress
+----------------
+A lot of things are in progress!
+
+- [████100%] Add Composer support
+- [▒▒▒▒ 0% ] Manage several langages
+- [▒▒▒▒ 0% ] Add command line support
+- [▒▒▒▒ 0% ] Plugin management
+- [▒▒▒▒ 0% ] Add unit tests
+- [▒▒▒▒ 0% ] Add proper documentation
+
+
+Licencing
+--------
+This piece of code is a free software under [LGPL v2.1](http://choosealicense.com/licenses/lgpl-v2.1/). See [LICENSE file](https://github.com/Fylhan/tchatbot/blob/master/LICENSE) for more information.
+
+To summarize:
+* Required
+  * License and copyright notice
+  * Disclose your modified Source if you re-distribute this software
+* Permitted
+  * Commercial Use
+  * Modification
+  * Distribution
+  * Sublicensing
+  * Patent Grant
+* Forbidden 	
+  * Hold Liable
--- index.php
+++ index.php
@@ -1,15 +1,29 @@
-<?php 
-require('../autoload.php');
+<?php
+require (__DIR__ . '/vendor/autoload.php');
+
+use Composer\Factory;
+use Composer\IO\BufferIO;
+use Composer\Repository\RepositoryManager;
+use Composer\DependencyResolver\Operation\UpdateOperation;
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8" />
-	<title>Platphorm</title>
-	<meta name="author" content="Fylhan" />
+<meta charset="utf-8" />
+<title>Platphorm</title>
+<meta name="author" content="Fylhan" />
 </head>
 <body>
 	<p>Hello World!</p>
+	<?php
+	$bufferIo = new BufferIO();
+	$composer = Factory::create($bufferIo);
+	$repo = $composer->getPackage()->getRepositories();
+	$repoConfig = $repo[0];
+	$package = $composer->getPackage();
+	var_dump($composer->getRepositoryManager()->createRepository("vcs", $repoConfig)->getPackages());
+	var_dump($composer->getInstallationManager()->update($composer->getRepositoryManager()->createRepository("vcs", $repoConfig)->getPackages(), new UpdateOperation($package, $package)));
+	?>
 </body>
 </html>
